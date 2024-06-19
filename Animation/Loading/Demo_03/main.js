e573af38a51b404c8ff29a565d5d99346ed73188
Vinhdev04 <pcv.fed@gmail.com>
2024-06-19 16:12:04 +0700

    upload source code Animation Loading P2 and P3

---
+++ Animation/Loading/Demo_03/main.js
@@ -0,0 +1,18 @@
+let outer = document.querySelector(".outer");
+let inner = document.querySelector(".inner");
+let percent = document.querySelector("span");
+let count = 0;
+inner.addEventListener("click", function () {
+  let loading = setInterval(animate, 200);
+  function animate() {
+    if (count == 100) {
+      outer.classList.remove("active-loader");
+      outer.classList.add("active-loader_2");
+      clearInterval(loading);
+    } else {
+      count += 1;
+      percent.textContent = count + "%";
+      outer.classList.add("active-loader");
+    }
+  }
+});
